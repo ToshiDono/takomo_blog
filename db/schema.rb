10cc5042bba6ad3c2edc4a0480ba778d17c02f2d
Anton <anton@ondoc.me>
2017-11-13 19:19:58 +0300

    Create setings for Pg DB

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171112141629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.text "text"
